548f23db1d2cf1a953398ce52eb52d3e38a128d2
Ludwig Wiedemann (PT-331) <Ludwig.Wiedemann@bmw.de>
2021-01-15 12:23:27 +0100

    Day 07 part 1 fertig mit Olaf

--- src/AdventOfCodePcg/Day07.java
+++ src/AdventOfCodePcg/Day07.java
@@ -2,52 +2,97 @@ package AdventOfCodePcg;
 
 import Shared.FileLoader;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Day07 {
     List<String> input;
-
+    private static final Map<String, List<String>> rules = new HashMap<>();
 
     public Day07() {
-        this.input = FileLoader.loadFile("puzzles/Day07Test.txt");
+        this("puzzles/Day07Test.txt");
     }
 
     public Day07(String path) {
         this.input = FileLoader.loadFile(path);
+        // key - value
+        // "light red" - "bright white", "muted yellow"
+
+        for (String rule : input) {
+            String[] kv = rule.split(" bags contain ");
+
+            String[] contains = kv[1]
+                    .replace(" bags", "")
+                    .replace(" bag", "")
+                    .replace(".", "")
+                    .replace("no other", "")
+                    .split(", ");
+
+            if (!contains[0].isEmpty())
+                rules.put(kv[0], Arrays.asList(contains));
+        }
+
+        System.out.println(rules);
+    }
+
+    public void part1() {
+        int count = 0;
+        for (String color : rules.keySet()) {
+            boolean containsShinyGold = contains(color, "shiny gold");
+            System.out.println(color + " " + containsShinyGold);
+            if (containsShinyGold) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    private boolean contains(String color, String searchColor) {
+        List<String> contained = rules.getOrDefault(color, Collections.emptyList());
+        for (String c : contained) {
+            String[] countAndColor = c.split(" ", 2);
+            String containedColor = countAndColor[1];
+            if (containedColor.equals(searchColor))
+                return true;
+            if (contains(containedColor, searchColor))
+                return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        List<String[]> bags = new ArrayList<>();
-        List<String> contains = new ArrayList<>();
-
-        Day07 d7 = new Day07();
-        Iterator<String> iter = d7.input.iterator();
-        while(iter.hasNext()) {
-            String rule = iter.next();
-            bags.add(d7.splitRule(rule));
-        }
+        Day07 d7 = new Day07("puzzles/Day07.txt");
+        d7.part1();
+    }
 
 
-
-        for(int i = 0; i < bags.size(); i++) {
-            if(bags.get(i)[1].contains("shiny gold")){
-
-                contains.add(bags.get(i)[0]);
-                bags.remove(bags.get(i));
-            }
-        }
-        System.out.println(contains);
+//        List<String[]> bags = new ArrayList<>();
+//        List<String> contains = new ArrayList<>();
+//
+//        Iterator<String> iter = d7.input.iterator();
+//        while(iter.hasNext()) {
+//            String rule = iter.next();
+//            bags.add(d7.splitRule(rule));
+//        }
+//
+//
+//
+//        for(int i = 0; i < bags.size(); i++) {
+//            if(bags.get(i)[1].contains("shiny gold")){
+//
+//                contains.add(bags.get(i)[0]);
+//                bags.remove(bags.get(i));
+//            }
+//        }
+//        System.out.println(contains);
 
 //
 //        }
 
-    }
+//}
 
-    String[] splitRule(String rule){
-        return rule.split("contain");
-    }
+//    String[] splitRule(String rule){
+//        return rule.split("contain");
+//    }
 
 //    public static void main(String[] args) {
 //        Day07 d7 = new Day07("puzzles/Day07.txt");
